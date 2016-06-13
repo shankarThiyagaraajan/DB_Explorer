6c41487f01bcf850d7e1806ac94acd521d8e3bda
Shankar Thiyagaraajan <shankarthiyagaraajan@gmail.com>
2016-06-13 23:53:49 +0530

    Reference Commit

---
+++ src/Facades/OTF.php
@@ -0,0 +1,12 @@
+<?php
+namespace shankarbala33\db_explorer\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class OTF extends Facade{
+
+    protected static function getFacadeAccessor()
+    {
+        return new \App\Helper\OTF();
+    }
+}
