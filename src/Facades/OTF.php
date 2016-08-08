8386ade2824a2215160e43b27091560dfa08ab47
Shankar Thiyagaraajan <shankarthiyagaraajan@gmail.com>
2016-08-08 04:18:30 -0400

    Applied fixes from StyleCI

--- src/Facades/OTF.php
+++ src/Facades/OTF.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace shankarbala33\db_explorer\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class OTF extends Facade{
-
+class OTF extends Facade
+{
     protected static function getFacadeAccessor()
     {
         return new \App\Helper\OTF();
